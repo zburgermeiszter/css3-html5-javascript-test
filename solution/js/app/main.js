7676d3930cf3d96e71d8f51d612e39ceff700a38
zburgermeiszter <zoltan@burgermeiszter.com>
2015-12-15 21:37:46 +0000

    JS done

--- solution/js/app/main.js
+++ solution/js/app/main.js
@@ -1,5 +1,41 @@
-define(["jquery"], function($) {
-    $(function() {
-        console.log("Done");
+define(["jquery"], function ($) {
+
+    var inc = function (number, max) {
+        if (max === undefined) {
+            max = 10;
+        }
+
+        number++;
+
+        return (number > max) ? max : number;
+    };
+
+    var dec = function (number, min) {
+        if (min === undefined) {
+            min = 0;
+        }
+
+        number--;
+
+        return (number < min) ? min : number;
+    };
+
+    $(function () {
+
+        var counters = $("article aside .counter");
+
+        counters.each(function () {
+
+            var counter = $(this);
+
+            counter.siblings(".up").click(function () {
+                counter.text(inc(counter.text()));
+            });
+
+            counter.siblings(".down").click(function () {
+                counter.text(dec(counter.text()));
+            });
+        });
+
     });
 });
